6afdabffab68d74a7312514dfb3fb262a8a7d9e2
Vikram Nitin <vikramnitin9@gmail.com>
2020-01-19 16:28:19 -0500

    Ready for Heroku

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,64 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
+
   def initialize(word)
-    @word = word
+    @word           = word
+    @guesses        = ''
+    @wrong_guesses  = ''
+  end
+  attr_reader :word
+  attr_reader :guesses
+  attr_reader :wrong_guesses
+
+  def guess(letter)
+      if not letter =~ /^[a-zA-Z]$/
+        raise ArgumentError, 'Argument is not a letter'
+      end
+      letter = letter.downcase
+      if @word.include? letter
+        if not @guesses.include? letter
+          @guesses << letter
+          return true
+        else
+          return false
+        end
+      else
+        if not @wrong_guesses.include? letter
+          @wrong_guesses << letter
+          return true
+        else
+          return false
+        end
+      end
+  end
+
+  def guess_several_letters(letters)
+    letters.each do |letter|
+      guess(letter)
+    end
+  end
+
+  def word_with_guesses
+    wg = ''
+    @word.each_char do |letter|
+      if @guesses.include? letter
+        wg << letter
+      else
+        wg << '-'
+      end
+    end
+    return wg
+  end
+
+  def check_win_or_lose
+    if @word.each_char.all? {|letter| @guesses.include? letter}
+      return :win
+    elsif @wrong_guesses.length >= 7
+      return :lose
+    else
+      return :play
+    end
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
